dadb3c2ac989332a77d6514889daa4073da231e4
afonsosgsramalho <afonsosgsramalho@gmail.com>
2024-02-10 16:39:11 +0000

    db fixed and first select

--- src/wslpipe/utils/db.py
+++ src/wslpipe/utils/db.py
@@ -1,29 +1,40 @@
 import psycopg2
 
-class Connection:
-    def __init__(self, user, dbname, password, host):
-        self.user = user
-        self.dbname = dbname
-        self.password = password
-        self.host = host
-        self.cursor = None
 
+class DB:
+    def __init__(self, user, dbname, password, host):
+        self.connection = None
+        try:
+            self.connection = psycopg2.connect(
+                user=user,
+                dbname=dbname,
+                password=password,
+                host=host,
+                port='5432'
+            )
+        except Exception as e:
+            print(f"The connection was not established: {e}")
+
+    def get_connection(self):
+        if self.connection is not None:
+            return self.connection
+        else:
+            raise Exception("Database connection is not established.")
 
     def connect_cursor(self):
         try:
-            connection = psycopg2.connect(
-                user=self.user,
-                dbname=self.dbname,
-                password=self.password,
-                host=self.host
-            )
-            self.cursor = connection.cursor()
-
+            return self.get_connection().cursor()
         except Exception as e:
-            print(f"The connection was not established: {e}")
-        
+            print(f"Failed to create a cursor: {e}")
+            raise e
+
+    def commit(self):
+        if self.connection is not None:
+            self.connection.commit()
+        else:
+            raise Exception("Database connection is not established.")
 
     def close_connection(self):
-        return self.cursor.close()
-
-
+        if self.connection is not None:
+            self.connection.close()
+            self.connection = None
--- src/wslpipe/wsl.py
+++ src/wslpipe/wsl.py
@@ -18,43 +18,43 @@ class Wsl:
     def get_athlete(self, html):
         try:
             soup = BeautifulSoup(html, 'html.parser')
+
             name = soup.find('div', {'class': 'avatar-text-primary'}).text
             country = soup.find('div', {'class': 'country-name'}).text
             bio = soup.find('div', {'class': 'new-athlete-bio-stats'})
             value = bio.find_all('div', {'class': 'value'})
 
-            values = []
+            values = [] 
 
             for v in value:
                 values.append(v.text.strip())
 
-            _, _, age, height, weight, hometown = values
+            stance, _, age, height, weight, hometown = values
 
-            return [name, country, int(age[:2]), height[-6:], weight[-5:], hometown]
+            return [name, country, stance, int(age[:2]), height[-6:], weight[-5:], hometown]
         
         except:
             return f'Athlete {name} could not be processed'
-        
+
     def get_competition(self, html):
         try:
             soup = BeautifulSoup(html, 'html.parser')
+
             event = soup.find('h1', {'class': 'event-information__title'}).text
             data_range = soup.find('span', {'class': 'event-information__meta-item event-information__meta-item--date-range'}).text
             start_date = data_range.split('-')[0] + data_range[-4:].strip()
-            start_date_object = datetime.strptime(
-                start_date, '%b %d %Y').date()
+            start_date_object = datetime.strptime(start_date, '%b %d %Y').date()
             end_date = data_range.split('-')[1].replace(',', '').strip()
-            end_date_object = datetime.strptime(
-                end_date, '%b %d %Y').date()
+            end_date_object = datetime.strptime(end_date, '%b %d %Y').date()
             state = soup.find('span', {'class': 'status-module__status'}).text
 
             return [event, start_date_object, end_date_object, state]
 
         except:
             return f'Event {event} could not be processed'
-
+        
     def get_ranking(self, year):
-        url = f'https://www.worldsurfleague.com/athletes/tour/mct?year={year}'
+        url = f'https://www.worldsurfleague.com/athletes/tour/mct?year={str(year)}'
         df = pd.read_html(url)[0]
 
         number_columns = len(df.columns)
@@ -67,22 +67,23 @@ class Wsl:
                             var_name='Number', value_name='Score')
 
         return df_melted
+    
 
     def get_last_update_ranking(self, html):
         soup = BeautifulSoup(html, 'html.parser')
+
         month, day, year = ' '.join(str(soup.find('div', {'class': 'athletes-tour-intro__notes'}).find_all_next('p')[0])
                                     .replace('<p>', '').replace('</p>', '').replace(',', '').split()[3:]).split()
-
         date_string = f'{day}/{month}/{year}'
         date_object = datetime.strptime(date_string, "%d/%B/%Y").date()
 
         return date_object
     
 
-wsl = Wsl()
-# html = wsl.fetch_page('/athletes/tour/mct?year=2023')
-# print(wsl.get_last_update_ranking(html))
-# html2 = wsl.fetch_page('/athletes/10192/lucas-vicente')
-html3 = wsl.fetch_page('/events/2024/ct/196/lexus-pipe-pro/main')
-print(wsl.get_competition(html3))
+# wsl = Wsl()
+# print(wsl.get_ranking(2023))
+# html2 = wsl.fetch_page('/athletes/3962/barron-mamiya')
+# print(wsl.get_athlete(html2))
+# html3 = wsl.fetch_page('/events/2024/ct/196/lexus-pipe-pro/main')
+# print(wsl.get_competition(html3))
 
